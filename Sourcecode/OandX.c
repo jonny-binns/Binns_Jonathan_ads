382522f71201ccfeefa7a0b84c3e3a898d37e574
Jonathan Binns <40311703@live.napier.ac.uk>
2019-03-12 15:31:59 +0000

    Added: addMove() Need to fix input validation

--- Sourcecode/OandX.c
+++ Sourcecode/OandX.c
@@ -38,41 +38,53 @@ void display(char* array)
 void insert(char* array, int pos, char move)
 {
     int idx;
-
-    for(idx = 8; idx>= pos; idx--)
+    if(move = 'X' || move = 'O')
     {
-        array[idx] = array[idx];
+      for(idx = 8; idx>= pos; idx--)
+      {
+          array[idx] = array[idx];
+      }
+        array[idx] = move;
     }
-    array[idx] = move;
+    else
+    {
+       return;
+    }
 }
 
-void CheckWinner(char* array)
+void addMove(char* array)
 {
-      if(array[0] && array[1] && array[2] == 'O' )
-      {
-          int winner = 1;
-      }
+    int position;
+    char move;
+
+    printf("Enter a position and a move: ");
+    scanf("%d%s", &position, &move);
+    printf("\n");
+
+    //random char insertion
+    if(array[position-1] = NULL)
+    {
+		    insert(array, position, move);
+        display(array);
+    }
+    else
+    {
+        printf("Error: Space Already Taken \n");
+        return;
+    }
 }
+
 
 int main()
 {
     instructions();
 
     char board[8];
-    int position;
-    char move;
-
     initialize(board);
+    //display(board);
 
-
-    printf("Enter a position and a move: ");
-    scanf("%d %s", &position, &move);
-    printf("\n");
-    insert(board, position, move);
-
-
-
-    display(board);
+    addMove(board);
+    addMove(board);
 
     return 0;
 }
